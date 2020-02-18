cdaeaf89e967b3073aafc419fce86eb417fb2941
Alec Wells <alec.wells47@gmail.com>
2020-02-18 08:48:23 -0700

    Write tests for event attributes

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
-describe 'test test' do
-  it "can test" do
+describe Event, type: :model do
 
-    expect(2).to eq(2)
+
+  describe 'attributes' do
+    it 'has attributes' do
+      @event = create(:event, name: 'first event', details: 'this is a test')
+
+      expect(@event.name).to eq('first event')
+      expect(@event.details).to eq('this is a test')
+    end
   end
 end
