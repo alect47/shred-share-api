e1c62f4e15295a4f76fac31dbd1d737c0c219485
Alec Wells <alec.wells47@gmail.com>
2020-02-18 09:49:08 -0700

    Finish testing for GET events

--- spec/requests/api/v1/events/get_event_spec.rb
+++ spec/requests/api/v1/events/get_event_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 describe "event api" do
   it "user can get events", :vcr do
+    create_list(:event, 3)
+
     get "/api/v1/events"
 
     expect(response).to be_successful
 
     events = JSON.parse(response.body)
-
-    expect(events["data"]["attributes"]).to eq(39.7392358)
+    
+    expect(events["data"][0]["attributes"]['name']).to eq('MyString')
+    expect(events["data"].count).to eq(3)
   end
 end
