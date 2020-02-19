bc1e3656efdd574b01ce83d5d19158a5626fa297
Alec Wells <alec.wells47@gmail.com>
2020-02-19 14:07:30 -0700

    Add create action to events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::EventsController < ApplicationController
     render json: EventSerializer.new(Event.all)
   end
 
+  def create
+    prms = event_params
+    @event = Event.new(event_params)
+    if @event.save
+        render json: EventSerializer.new(@event)
+    else
+      render json: { errors: "missing field"}, status: 401
+    end
+    # binding.pry
+    # event_params = json_parse(request)
+  end
+
+private
+
+  def event_params
+    # binding.pry
+    params.require(:event).permit(:name, :details)
+  end
 end
