799087c13d4fb4b69f19c78167392724756363d3
Alec Wells <alec.wells47@gmail.com>
2020-02-24 11:50:09 -0700

    Make migration for user

---
+++ db/migrate/20200224183637_create_user.rb
@@ -0,0 +1,19 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :user_name
+      t.string :google_token
+      t.string :google_refresh_token
+      t.string :picture
+      t.boolean :verified, default: false
+      t.datetime :google_oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end
