88520f097c8239e4e8019531f2fb9b64258707e1
Alec Wells <alec.wells47@gmail.com>
2020-02-24 11:48:41 -0700

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :events
+  has_many :user_events
+  has_many :events, through: :user_events
   validates_presence_of :user_name
   validates_presence_of :email
 
