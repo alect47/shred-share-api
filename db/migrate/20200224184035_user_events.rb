ccaad8e6be219d33ec1d2077aacebc5dfbb7cf3d
Alec Wells <alec.wells47@gmail.com>
2020-02-24 11:50:27 -0700

    Make migration for user_events

---
+++ db/migrate/20200224184035_user_events.rb
@@ -0,0 +1,10 @@
+class UserEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :user_events do |t|
+
+      t.references :user, foreign_key: true
+      t.references :event, foreign_key: true
+      t.boolean :cancelled, default: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_193148) do
+ActiveRecord::Schema.define(version: 2020_02_24_184035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,30 @@ ActiveRecord::Schema.define(version: 2020_02_16_193148) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_events", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "event_id"
+    t.boolean "cancelled", default: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "google_token"
+    t.string "google_refresh_token"
+    t.string "picture"
+    t.boolean "verified", default: false
+    t.datetime "google_oauth_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
 end
