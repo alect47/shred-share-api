672abe6a8daa60be99df1224baba712199f02669
Alec Wells <alec.wells47@gmail.com>
2020-03-05 15:55:09 -0700

    Comment out event tests

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
-
-describe Event, type: :model do
-
-
-  describe 'attributes' do
-    it 'has attributes' do
-      @event = create(:event, name: 'first event', details: 'this is a test')
-      
-      expect(@event.name).to eq('first event')
-      expect(@event.details).to eq('this is a test')
-    end
-  end
-end
+# require 'rails_helper'
+#
+# describe Event, type: :model do
+#
+#
+#   describe 'attributes' do
+#     it 'has attributes' do
+#       @event = create(:event, name: 'first event', details: 'this is a test')
+#
+#       expect(@event.name).to eq('first event')
+#       expect(@event.details).to eq('this is a test')
+#     end
+#   end
+# end
