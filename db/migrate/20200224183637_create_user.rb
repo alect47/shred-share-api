9ad34a28ef9b172396a8fe77f7a6aeb355f3a7df
Alec Wells <alec.wells47@gmail.com>
2020-03-05 15:30:49 -0700

    Change schema for new project direction

--- db/migrate/20200224183637_create_user.rb
+++ db/migrate/20200224183637_create_user.rb
@@ -1,17 +1,12 @@
 class CreateUser < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :provider
-      t.string :uid
       t.string :email
-      t.string :first_name
-      t.string :last_name
+      t.string :password_digest
+      t.string :pass
+      t.string :name
       t.string :user_name
-      t.string :google_token
-      t.string :google_refresh_token
-      t.string :picture
-      t.boolean :verified, default: false
-      t.datetime :google_oauth_expires_at
+      t.string :phone_number
 
       t.timestamps
     end
